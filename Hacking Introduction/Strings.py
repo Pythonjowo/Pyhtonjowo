421e3eeef1991617fbeee5f7a3f57b896ee8cdd3
psfauzi <psfauzi@gmail.com>
2020-10-29 17:28:45 +0700

    Fundamental Awal

---
+++ Hacking Introduction/Strings.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+#Print String
+
+print("Hello, World") #double quote
+print('\n') #new line
+print('Hello, Wolrd') #single Quotes
+
+print("""This is String 
+Multiples Lines! #triple quote for multi-line
+""")
+print("This is Strig "+"is Awsome") #we can also concatinate
