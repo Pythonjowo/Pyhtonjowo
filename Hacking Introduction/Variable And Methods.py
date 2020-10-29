3d4d59cdef32c1f2a631ac51dad3e11161534715
psfauzi <psfauzi@gmail.com>
2020-10-29 18:11:36 +0700

    Fundamental Awal

---
+++ Hacking Introduction/Variable And Methods.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+#variables and Methods
+
+quote =  "All is fair in love and war."
+print(quote.upper()) #Uppercase
+print(quote.lower()) #Lowercase
+print(quote.title()) #Title Case
+
+print(len(quote))
+
+name = "Health" #String
+age = 30 #Integer int(30)
+gpa = 3.7 #float float(3.7)
+
+print(int(age))
+print(int(30.9))
+print('\n')
+print ("My Name is " + name +  " and I am " + str(age) + " Years Old.")
+
+age += 1
+print(age)
+
+birthday = 1
+age += birthday
+print (age)
+
