060ad82297852f38d9138465b0668c517d66dcc4
psfauzi <psfauzi@gmail.com>
2020-10-29 23:55:36 +0700

    Fundamental Awal

---
+++ Hacking Introduction/functions.py
@@ -0,0 +1,33 @@
+
+#Functions
+print("Here is an Example Function")
+def who_am_i():
+    name = "Health "
+    age = 30
+    print('hay ' + name  +  str(age))
+who_am_i()
+
+
+#adding parameters
+def add_one_hundred (num):
+    print(num+100)
+add_one_hundred(100)
+
+#multiple parameters
+def add (x,y) :
+    print(x + y)
+
+add(7,7)
+
+def multiply(x,y):
+    return x * y
+print(multiply (7,7))
+
+def square_root(x):
+    print(x ** .5)
+square_root(64)
+
+def nl():
+    print('\n')
+nl()
+
