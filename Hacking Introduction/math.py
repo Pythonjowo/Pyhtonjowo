39bdaf473484acfe0860f4bc0c787c1cdc52eb5e
psfauzi <psfauzi@gmail.com>
2020-10-29 17:44:49 +0700

    Fundamental Awal

---
+++ Hacking Introduction/math.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#math
+print(50+50) #add
+print(50-50) #subtract
+print(50*50) #multiple
+print(50/50) #devide
+print(50*50) #multiple
+print(50+50-50*50/50) #PEMDAS
+print(50**50) #exponents
+print(50%6) #modulo
+print( 50/6)
+print ( 50 //6) # no leftovers
