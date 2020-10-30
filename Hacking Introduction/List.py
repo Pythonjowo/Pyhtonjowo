55bba197991c2497b00106af6d269d392e2f9a82
psfauzi <psfauzi@gmail.com>
2020-10-30 16:32:45 +0700

    Fundamental Awal

---
+++ Hacking Introduction/List.py
@@ -0,0 +1,22 @@
+
+#list - Have Brackets []
+movies = ["When Harry Met Sally", " The HAngover", "The Perks Of Being a Wallflower", "The Exostixs"]
+
+print(movies[1]) #return the seond item
+print(movies[0]) #returns the first item in the list
+
+print(movies[1:4])
+print(movies[1:])
+print(movies[:2])
+print(movies[3])
+print(len(movies))
+movies.append("JAWS")
+print(movies)
+
+movies.pop()
+print(movies)
+
+movies.pop(0)
+print(movies)
+
+
