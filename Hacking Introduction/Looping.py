98efe25058f12436d6868d33c4c77a4be37dc642
psfauzi <psfauzi@gmail.com>
2020-10-30 16:41:48 +0700

    Fundamental Awal

---
+++ Hacking Introduction/Looping.py
@@ -0,0 +1,12 @@
+#Looping
+
+#For Loops - Start to finish of an iterate
+vegetables = ["cocumber","spinach","cabbage"]
+for x in vegetables:
+    print(x)
+
+#While Loop - Execute as long as True
+i = 1
+while i < 10:
+    print(i)
+    i += 1
