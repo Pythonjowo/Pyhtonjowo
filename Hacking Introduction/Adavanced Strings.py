2d9398b147dd05cc6065e8b641be112c7f269bd0
psfauzi <psfauzi@gmail.com>
2020-11-01 13:24:30 +0700

    Fundamental Awal

---
+++ Hacking Introduction/Adavanced Strings.py
@@ -0,0 +1,28 @@
+
+my_name = "health"
+print(my_name[0])
+print(my_name[-1])
+
+sentence = "This is a sentence ."
+print(sentence[:4])
+
+print(sentence.split())
+
+sentence_split = sentence.split()
+sentence_join = ' '.join(sentence_split)
+print(sentence_join)
+
+quote = "He said, \"give me all your money\""
+print(quote)
+
+too_much_space = "hello"
+print(too_much_space)
+
+print('a' in "apple")
+letter = "A"
+word = "apple"
+print(letter.lower() in word.lower() )#Improved
+
+movie = "The Hangeover"
+print("My Favorite Movie is {}. ".format(movie))
+
