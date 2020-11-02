1ad89c1d633b91d72242ca8656b4474fa231b9ad
psfauzi <psfauzi@gmail.com>
2020-11-02 21:44:42 +0700

    Fundamental Awal

---
+++ Fundamental Satu #1/Tipe Data.py
@@ -0,0 +1,34 @@
+#tipe data Boolean
+print(True)
+
+#tipe data String
+print("Ayo Belajar Python")
+print('Belajar Python Sangat Mudah')
+
+#tipe data Integer
+print(20)
+
+#tipe data float
+print(3.14)
+
+#tipe Hexadesimal
+print(9a)
+
+#tipe data complex
+print (5j)
+
+#tipe data list
+print([1,2,3,4,5])
+print(["Satu","Dua","tiga"])
+
+#tipe data Tuple
+print((1,2,3,4,5))
+print(("Satu","Dua","Tiga"))
+
+#tipe data Dictionary
+print({"nama":"Budi", 'Umur':21})
+
+#Tipe data dictionary dimasukkan ke variable biodata
+biodata = {"nama": "Andi", 'umur':21} # proses inisaliasasi biodata
+print(biodata) # proses mencetak varibale dari biodata
+print(type(biodata)) # fungsi untuk melihat jenis dari variable
