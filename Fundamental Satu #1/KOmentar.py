4b225b58a5cdb7d469fe100128799c7772e64524
psfauzi <psfauzi@gmail.com>
2020-11-02 21:33:37 +0700

    Fundamental Awal

---
+++ Fundamental Satu #1/KOmentar.py
@@ -0,0 +1,25 @@
+#Ini adalah Komentar
+# Tulisan ini tidak akan diekseksui
+
+#KOmenter dengan tanda pagar hanya bisa digunakan
+#untuk
+#satu
+#baris
+
+"""
+Penulisan KOmentar lebih dari satu baris yaitu
+dengan menggunakan kutip dua 3 kali dan
+ditutup
+"""
+
+print("Hello World") #ini juga komentar
+
+#print("Welcome")
+
+# komentar bisa berisi spesial karakter !@#$^&*(),./;'[]\
+
+#mencetak nama
+print("Bundi")
+
+#mencetak angka/integer
+print(123)
