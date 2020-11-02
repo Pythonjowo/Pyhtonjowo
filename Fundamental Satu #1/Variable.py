8d678b3acc65cced7b3abc5e3d0916a9f99c1307
psfauzi <psfauzi@gmail.com>
2020-11-02 21:57:21 +0700

    Fundamental Awal

---
+++ Fundamental Satu #1/Variable.py
@@ -0,0 +1,31 @@
+nama = "Jhon Doe"
+
+print(nama)
+
+umur = 20
+print(umur)
+print(type(umur)) #mencetak tipe data
+print('\n')
+umur = "Dua puluh satu"
+print(umur)
+print(type(umur))
+print('\n')
+
+namaDepan = "Budi"
+namaBelakang = "Susanto"
+nama = namaDepan + "" + namaBelakang
+umur = 22
+hobi = "Berenang"
+print("Biodata\n" + nama + "\n"+ str(umur) + "\n" + hobi)
+
+#contoh variable lainya
+inivariable = "Halo"
+ini_juga_variable = "Hai"
+_inivariablejuga = "Hi"
+inivariable222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print("\n")
+print(luas)
