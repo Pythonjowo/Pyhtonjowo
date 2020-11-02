0f27577aa6b48bb45a9d9ff5c83f96704e12a643
psfauzi <psfauzi@gmail.com>
2020-11-02 23:09:08 +0700

    Fundamental Awal

---
+++ Fundamental Satu #1/Operator.py
@@ -0,0 +1,43 @@
+#Operator Aritmatika
+
+#Penjumlah
+print(13 + 2)
+apel = 7
+jeruk = 9
+buah = apel  + jeruk
+print(buah)
+
+#pengurangan
+hutang = 10000
+bayar = 5000
+sisaHutang = hutang - bayar
+print("Sisa hutang Anda Adalah ", sisaHutang)
+
+#Perkalian
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print(luas)
+
+#Pembagian
+kue = 16
+anak = 4
+kuePerAnak = kue / anak
+print(kuePerAnak)
+
+#sisa Bagi /Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil     = bilangan1 % bilangan2
+print("Sisa bagi dari bilangan " + str(bilangan1) + " dan " + str(bilangan2) + " adalah " + str(hasil) )
+
+#pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print(hasilPangkat)
+
+#Pembagian Bulat
+print(10//3)
+#10 dibagi 3 adalah 3.3333. Karena dibulatkan maka akan menghasilkan nilai 3.
+
