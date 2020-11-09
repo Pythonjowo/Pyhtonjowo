7033717a1459a1f43b92593dda78ed8237aadd74
psfauzi <psfauzi@gmail.com>
2020-11-08 22:45:32 -0800

    Kondisi.py

---
+++ Fundamental Satu #1/Kondisi.py
@@ -0,0 +1,40 @@
+# kondisi if adalah kondisi dimana yang akan dieksekusi jika bernilai benar
+
+nilai = 9
+
+#JIKA KONDISI BENAR TRUE maka program akan mengeksekusi perintah dibawahnya.
+if(nilai > 7):
+    print("Selamat Anda Lulus")
+
+#jika kondisi salah/False maka program tidak akan mengeksekusi perintah dibawahnya.
+if(nilai > 10):
+    print("Selamat Anda Lulus")
+else:
+    print("Caoumlaude")
+
+#Kondisi If Else
+nilai = 3
+
+if (nilai > 7):
+    print("Selmat Anda Lulus Ujian")
+else:
+    print("Maaf Anda Tidak Lulus")
+
+#Kondisi Elif
+hari_ini = "Minggu"
+
+if (hari_ini == "senin"):
+    print("Saya akan Kuliah")
+elif(hari_ini == "Selasa"):
+    print("Saya Akan Kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya Akan Kuliah")
+elif(hari_ini == "Kamis"):
+    print("Saya Akan Kuliah")
+elif(hari_ini == "Jum'at"):
+    print("Saya Akan Kuliah")
+elif(hari_ini == "Sabtu"):
+    print("Saya Akan Kuliah")
+elif(hari_ini == "Minggu"):
+    print("Saya Akan Kuliah")
+
