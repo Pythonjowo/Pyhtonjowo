d4f08f486cd4fb055f4f5edb2431640856137f5b
psfauzi <psfauzi@gmail.com>
2020-11-09 19:16:13 +0700

    List

---
+++ Fundamental Satu #1/LIst.py
@@ -0,0 +1,26 @@
+#Contoh sederhana pembuatan list pada bahasa pemrograman python
+
+list1 = ['kimia','fisika', 1993, 2017]
+list2 = [1,2,3,4,5]
+list3 = ["a","b","c","d"]
+
+#Akses nilai dalam list
+print("List[0]", list1[0])
+print("List[1:5", list2[1:5])
+
+#Update nilai dalam List
+print("Nilai ada pada index 2 :", list1[2])
+
+list1.insert(1,"hayo")
+print("Nilai baru ada pada index 2 : ", list1)
+
+
+#Menghapus List
+list = ["Rupawan", "ganteng","tampan"]
+list.remove("ganteng")
+print("Setelah dihapus pada index 2", list)
+
+
+#Operasi Dasar pada List Paython
+
+
