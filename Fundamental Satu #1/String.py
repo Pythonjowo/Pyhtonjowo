b8c9654847ecf2c81a73b3bf391cc176a85777f0
psfauzi <psfauzi@gmail.com>
2020-11-09 18:48:30 +0700

    String

---
+++ Fundamental Satu #1/String.py
@@ -0,0 +1,11 @@
+print("Hello World")
+#Mengakses nilai dan string
+name = 'Jhon Doe'
+message = "John Doe belajar bahasa python di BelajarPython"
+print("name[0]:", name[0])
+print("Message[1:4]:", message[1:4])
+
+#Mengupdate Data String
+
+message = 'Hello World'
+print("Update String :-", message[:6] + 'Python')
