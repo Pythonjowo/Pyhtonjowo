57cd2e208c996af3f514fb58ac02d3280e37a4a0
psfauzi <psfauzi@gmail.com>
2020-11-18 21:47:44 +0700

    Strings

---
+++ Kelas Terbuka 2017/Strings.py
@@ -0,0 +1,23 @@
+text = " Hai aku Ganteng"
+text1 = "jalan - jalan dihari jum'at"
+print(text, text1)
+
+text2 = 'alan - jalan dihari jum\'at'
+
+print(text2)
+
+text3 = """
+Text Ganteng
+Text Cantik
+Text Gagah
+"""
+
+print(text3)
+
+
+text4 =r'C:\nyoto'#Raw String
+
+print(text4)
+
+
+print(5*'wk')
