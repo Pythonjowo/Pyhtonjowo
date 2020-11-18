19dbab93efc303342be4998e375cdbb253d94c1e
psfauzi <psfauzi@gmail.com>
2020-11-18 21:31:57 +0700

    Modulus

---
+++ Kelas Terbuka 2017/number.py
@@ -0,0 +1,24 @@
+a = 5
+b = 3
+c = 4.0
+
+#penjumlahan
+
+d = a + b
+print("Penjumlahan a + b = ", + d)
+
+#Pengurangan
+
+print("Pengurangan a - b = ", + a - b)
+
+#Pembagian
+
+print("Pembagian dari a : b = ", + a//b)
+
+#Perkalian
+
+print("Perkalian dari a * b = ", + a  * b)
+
+#Modulus
+
+print("Modulus dari a % b = ", + a % b)
